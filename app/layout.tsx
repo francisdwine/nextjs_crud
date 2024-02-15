2a40e315b4e0e966fa77658921c24edcccf97394
spearm1ntx <francisbendulo@gmail.com>
2024-02-15 10:12:28 +0800

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { PrimeReactProvider,PrimeReactContext } from "primereact/api";
 
+import "primeicons/primeicons.css";
+import "primereact/resources/primereact.min.css";
+import 'primereact/resources/themes/lara-light-indigo/theme.css'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <PrimeReactProvider>
+      <body className={inter.className}>
+        {children}
+      </body>
+      </PrimeReactProvider>
     </html>
   );
 }
